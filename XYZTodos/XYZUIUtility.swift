1b792da67dc1d7e82aa891bdb82a8af9e817d01f
cb Hoh <hohcheebin@gmail.com>
2020-10-28 16:14:26 -0400

    .

---
+++ XYZTodos/XYZUIUtility.swift
@@ -0,0 +1,16 @@
+//
+//  XYZUIUtility.swift
+//  XYZTodos
+//
+//  Created by Chee Bin Hoh on 10/28/20.
+//
+
+import UIKit
+
+struct TableViewSectionCell {
+    
+    let identifier: String
+    let title: String?
+    var cellList = [String]()
+    var data: Any?
+}
