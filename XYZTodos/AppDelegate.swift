2192766331a66f98fb6f7fefa13a8d47f18dcb77
cb Hoh <hohcheebin@gmail.com>
2020-11-17 09:39:47 -0500

    .

--- XYZTodos/AppDelegate.swift
+++ XYZTodos/AppDelegate.swift
@@ -67,7 +67,8 @@ class AppDelegate: UIResponder,
         return refreshTodos
     }
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         global = loadGlobalFromManagedContext();
@@ -94,14 +95,17 @@ class AppDelegate: UIResponder,
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
 
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    func application(_ application: UIApplication,
+                     didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
@@ -160,8 +164,8 @@ class AppDelegate: UIResponder,
     }
     
     func userNotificationCenter(_ center: UNUserNotificationCenter,
-          didReceive response: UNNotificationResponse,
-          withCompletionHandler completionHandler: @escaping () -> Void) {
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
         
         let scene = UIApplication.shared.connectedScenes.first
         
