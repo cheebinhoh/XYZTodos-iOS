455dd4269bbbac1d33943be80968f309b554ed0e
cb Hoh <hohcheebin@gmail.com>
2020-11-16 21:11:32 -0500

    .

--- XYZTodos/SceneDelegate.swift
+++ XYZTodos/SceneDelegate.swift
@@ -74,6 +74,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Save changes in the application's managed object context when the application transitions to the background.
         (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
-
 }
 
