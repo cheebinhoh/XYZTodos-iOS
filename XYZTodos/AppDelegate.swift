06ff495815538fb8ef6e32b29aac47813d1ab76d
cb Hoh <hohcheebin@gmail.com>
2021-01-02 12:27:52 -0500

    .

--- XYZTodos/AppDelegate.swift
+++ XYZTodos/AppDelegate.swift
@@ -144,42 +144,6 @@ class AppDelegate: UIResponder,
         return container
     }()
 
-    
-    lazy var persistentContainer: NSPersistentCloudKitContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-        */
-        //let persistentContainer = NSPersistentContainer(name: "Collect")
-        let storeURL = URL.storeURL(for: appGroup, databaseName: databaseName)
-        let storeDescription = NSPersistentStoreDescription(url: storeURL)
-
-        let container = NSPersistentCloudKitContainer(name: databaseName)
-        container.persistentStoreDescriptions = [storeDescription]
-        
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Exception: unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        
-        return container
-    }()
-
     // MARK: - Core Data Saving support
 
     func saveContext () {
@@ -267,16 +231,6 @@ func managedContextDeprecated() -> NSManagedObjectContext? {
     }
   
     return appDelegate.persistentContainerDeprecated.viewContext
-}
-
-func managedContext() -> NSManagedObjectContext? {
-    
-    guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-        
-        fatalError("Exception: AppDelegate is expected")
-    }
-    
-    return appDelegate.persistentContainer.viewContext
 }
 
 func saveManageContextDeprecated() {
--- XYZTodosWidget/XYZTodosWidget.swift
+++ XYZTodosWidget/XYZTodosWidget.swift
@@ -9,48 +9,15 @@ import WidgetKit
 import SwiftUI
 import CoreData
 
-var persistentContainer: NSPersistentCloudKitContainer = {
-    /*
-     The persistent container for the application. This implementation
-     creates and returns a container, having loaded the store for the
-     application to it. This property is optional since there are legitimate
-     error conditions that could cause the creation of the store to fail.
-    */
-    //let persistentContainer = NSPersistentContainer(name: "Collect")
-    let storeURL = URL.storeURL(for: appGroup, databaseName: databaseName)
-    let storeDescription = NSPersistentStoreDescription(url: storeURL)
-
-    let container = NSPersistentCloudKitContainer(name: databaseName)
-    container.persistentStoreDescriptions = [storeDescription]
-    
-    container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-        
-        if let error = error as NSError? {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-             
-            /*
-             Typical reasons for an error here include:
-             * The parent directory does not exist, cannot be created, or disallows writing.
-             * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-             * The device is out of space.
-             * The store could not be migrated to the current model version.
-             Check the error message to determine what the actual problem was.
-             */
-            fatalError("Exception: unresolved error \(error), \(error.userInfo)")
-        }
-    })
-    
-    return container
-}()
-
 struct Provider: TimelineProvider {
     
     func placeholder(in context: Context) -> SimpleEntry {
+        
         SimpleEntry(date: Date(), todos: [XYZTodo]())
     }
 
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        
         let entry = SimpleEntry(date: Date(), todos: [XYZTodo]())
         completion(entry)
     }
@@ -59,12 +26,12 @@ struct Provider: TimelineProvider {
         
         var entries: [SimpleEntry] = []
 
-        let todos = loadTodosFromManagedContext(persistentContainer.viewContext)
+        let todos = loadTodosFromManagedContext(managedContext())
         var todosInFutureOfToday = [XYZTodo]()
         var todosDue = [XYZTodo]()
         var overdues = [Bool]()
         let nowOnward = Date().getTimeOfToday()
-        
+
         for todo in todos! {
 
             if let dow = DayOfWeek(rawValue: todo.group),
